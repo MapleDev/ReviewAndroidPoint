9598fea975122e5baa8b88be5162a4965217eb22
MapleDev <zgh8126@qq.com>
2016-09-05 00:33:15 +0800

    新增一个类似 发廊加载栏 View

--- marqueeview/src/main/java/com/xznn/marqueeview/MainActivity.java
+++ marqueeview/src/main/java/com/xznn/marqueeview/MainActivity.java
@@ -1,9 +1,16 @@
 package com.xznn.marqueeview;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,22 +21,71 @@ public class MainActivity extends AppCompatActivity {
 
     private MarqueeView marqueeView;
     private ImageView mIv2;
+    private ImageView mFalang_fg;
+    private ImageView mFalang_fg2;
+    private FrameLayout mFf;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         marqueeView = (MarqueeView) findViewById(R.id.marquee_view);
-        initMarqueeView();
+        mFalang_fg = (ImageView) findViewById(R.id.falang_fg);
+        mFalang_fg2 = (ImageView) findViewById(R.id.falang_fg2);
+        mFf = (FrameLayout) findViewById(R.id.ff);
 
+        initMarqueeView();
+        startPropertyAnim();
+    }
+
+    // 动画实际执行
+    private void startPropertyAnim() {
+        // X轴方向上的坐标
+        float translationX = mFalang_fg.getTranslationX();
+
+        // 向右移动 500pix，然后再移动到原来的位置复原。
+        // 参数“translationX”指明在x坐标轴位移，即水平位移。
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        int screenWidth = wm.getDefaultDisplay().getWidth();
+
+        final ObjectAnimator anim = ObjectAnimator.ofFloat(mFalang_fg, "translationX", 0, screenWidth);
+        final ObjectAnimator anim2 = ObjectAnimator.ofFloat(mFalang_fg2, "translationX", -screenWidth, 0);
+
+        anim.setDuration(80000);
+        anim2.setDuration(80000);
+        anim.setRepeatCount(ValueAnimator.INFINITE);
+        anim2.setRepeatCount(ValueAnimator.INFINITE);
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                anim.start();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                super.onAnimationRepeat(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                anim2.start();
+            }
+        });
+
+        // 正式开始启动执行动画
+        anim.start();
     }
 
     private void setMarqueeViewVisibility() {
-            if(map.size() == 0) {
-                marqueeView.setVisibility(View.GONE);
-            } else {
-                marqueeView.setVisibility(View.VISIBLE);
-            }
+        if (map.size() == 0) {
+            mFf.setVisibility(View.GONE);
+        } else {
+            mFf.setVisibility(View.VISIBLE);
+        }
     }
 
     private void initMarqueeView() {
@@ -56,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public int ID = 10002;
+    public int ID = 10000;
     LinkedHashMap<Integer, View> map = new LinkedHashMap<>();
 
     public void onAdd(View v) {
@@ -68,13 +124,12 @@ public class MainActivity extends AppCompatActivity {
         map.put(ID++, inflate);
 
         setMarqueeViewVisibility();
-
     }
 
     public void onDel(View v) {
-        if (map.containsKey(10002)) {
-            marqueeView.removeViewInQueue(map.get(10002));
-            map.remove(10002);
+        if (map.containsKey(10000)) {
+            marqueeView.removeViewInQueue(map.get(10000));
+            map.remove(10000);
         }
         setMarqueeViewVisibility();
     }
